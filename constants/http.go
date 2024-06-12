3a00abddfa21b66247ef266b230f3bdf341b7798
vidhanarya <vidhanarya50@gmail.com>
2024-06-13 00:43:06 +0530

    Add CORS middleware

---
+++ constants/http.go
@@ -0,0 +1,25 @@
+package constants
+
+const (
+	HeaderXUserID         = "X-User-Id"
+	HeaderContentType     = "Content-Type"
+	HeaderXForwardedFor   = "X-FORWARDED-FOR"
+	HeaderXRequestTraceID = "X-Request-Trace-Id"
+	HeaderAuthorization   = "Authorization"
+
+	MethodGet     = "GET"
+	MethodPost    = "POST"
+	MethodPut     = "PUT"
+	MethodPatch   = "PATCH"
+	MethodHead    = "HEAD"
+	MethodOptions = "OPTIONS"
+	MethodDelete  = "DELETE"
+
+	ApplicationJSON = "application/json; charset=utf-8"
+)
+
+var (
+	AllowedHeaders = []string{HeaderXUserID, HeaderXRequestTraceID, HeaderContentType, HeaderXForwardedFor, HeaderAuthorization}
+	AllowedMethods = []string{MethodGet, MethodPost, MethodPut, MethodPatch, MethodHead, MethodOptions}
+	AllowedOrigins = []string{"*"}
+)
